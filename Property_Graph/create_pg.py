f88d95dda6f650c0ef4524071614991f2e2ec544
hanselguzman-soto <soto26938@gmail.com>
2022-08-21 16:44:09 -0700

    refactor to not create duplicate nodes

--- Property_Graph/create_pg.py
+++ Property_Graph/create_pg.py
@@ -9,12 +9,13 @@ class GraphDataCreator:
     def __init__(self, pathToRelationData):
         self.pathToRelationData = pathToRelationData
         self.df = pd.read_csv(self.pathToRelationData, sep="\t")
-        self.df.drop_duplicates() #investigate later 
+        # self.df.drop_duplicates() #investigate later 
+        # self.df.sort_values(by=["tablet_num"], inplace=True)
     
     def create_graph(self): #in the future, this will be create nodes, and then we create the graph
         neo4j_graph = Graph(password="password")
         for _, row in self.df.iterrows():
-            parsedRelationTypes = ParseRelationTypes(row.relation_type, row.subject, row.object, row.tablet_num, row.providence,
+            parsedRelationTypes = ParseRelationTypes(neo4j_graph, row.relation_type, row.subject, row.object, row.tablet_num, row.providence,
             row.period, row.dates_referenced, row.subject_tag, row.object_tag)
             neo4j_graph.create(parsedRelationTypes.Relationship)
         
--- Property_Graph/relations.py
+++ Property_Graph/relations.py
@@ -14,44 +14,63 @@ tablet_num: P100001
 
 '''
 
+#global variable
+unique_entity_dict = {}
+
 class ParseRelationTypes:
-    def __init__(self, relationType, subject_name, object_name, tablet_num, 
+    def __init__(self, graph, relationType, subject_name, object_name, tablet_num, 
                 providence, period, dates_referenced, subject_tag, object_tag) -> None:
+        self.graph = graph 
         self.relationType = str(relationType)  #person:delivers_animal 
-        self.Subject = self.createEntityNode(subject_tag, subject_name, tablet_num)
-        self.Object = self.createEntityNode(object_tag, object_name, tablet_num)
-        self.Relationship = self.createRelationshipNode(providence, period, dates_referenced)
+        self.Subject = self.createEntityNode(subject_tag, subject_name, tablet_num) #should be node objects
+        self.Object = self.createEntityNode(object_tag, object_name, tablet_num) # should be node objects
+
+
+        self.Relationship = self.createRelationshipNode(self.Subject, self.Object, providence, period, dates_referenced)
 
     def parse_relation(self):
         return self.relationType.split(":")
 
+    def checkIfEntityExists(self, entity_name):
+        existing_entity = self.graph.nodes.match('Person', name=entity_name).first()
+        if existing_entity is not None:
+            return existing_entity #node object
+
     def createEntityNode(self, entity_tag, entity_name, tablet_num):
         if 'PN' in entity_tag or 'FOR' in entity_tag:
-            return Person(entity_name, tablet_num)
+            existing_person = self.graph.nodes.match('Person',name=entity_name).first()
+            print(existing_person)
+            print(type(existing_person))
+            if existing_person is not None:
+                return existing_person
+            else:
+                return Person(entity_name, tablet_num).node
         elif 'ANIM' in entity_tag: 
-            return Animal(entity_name, tablet_num)
+            return Animal(entity_name, tablet_num).node
         
 
-    def createRelationshipNode(self, providence, period, dates_referenced):
-        return Relationship(self.Subject.get_neo4j_node(), self.relationType, self.Object.get_neo4j_node(), 
-        providence=providence, period=period, dates_referenced=dates_referenced)
+    def createRelationshipNode(self, SubjectNode, ObjectNode, providence, period, dates_referenced):
+        return Relationship(SubjectNode, self.relationType, ObjectNode, 
+        name=self.relationType, providence=providence, period=period, dates_referenced=dates_referenced)
 
 #PN - Personal Name Tag
 class Person:
     def __init__(self, name, tablet_num) -> None:
         self.name = str(name)
-        self.tablet_num = str(tablet_num)   
+        self.tablet_num = str(tablet_num)
+        self.node = Node("Person", name=self.name, tablet_num=self.tablet_num)    
 
-    def get_neo4j_node(self):
-        neo4j_node = Node("Person", pk=get_random_number(), name=self.name, tablet_num=self.tablet_num)   
-        return neo4j_node
+    def create_neo4j_node(self):
+        neo4j_node = Node("Person", name=self.name, tablet_num=self.tablet_num)   
+        self.node = neo4j_node
 
 
 class Animal:
     def __init__(self, name, tablet_num):
         self.name = str(name)
         self.tablet_num = str(tablet_num)
+        self.node =  Node('Animal', name=self.name, tablet_num=self.tablet_num)
   
     def get_neo4j_node(self):
-        neo4j_node = Node('Animal', pk=get_random_number(), name=self.name, tablet_num=self.tablet_num)
-        return neo4j_node
+        neo4j_node = Node('Animal', name=self.name, tablet_num=self.tablet_num)
+        self.node = neo4j_node
